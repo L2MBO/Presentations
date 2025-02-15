f7566f452486a2c8ea09da96588e79a32875693e
VechnoMolodoy123 <163846431+VechnoMolodoy123@users.noreply.github.com>
2025-02-15 15:11:36 +0300

    5

--- app/src/main/java/com/example/battletanks/enums/Material.kt
+++ app/src/main/java/com/example/battletanks/enums/Material.kt
@@ -2,9 +2,9 @@ package com.example.battletanks.enums
 
 import com.example.battletanks.enums.Material
 
-enum class Material {
-    EMPTY,
-    BRICK,
-    CONCRETE,
-    GRASS,
+enum class Material(val tankConGoThrough: Boolean) {
+    EMPTY(true),
+    BRICK(false),
+    CONCRETE(false),
+    GRASS(true),
 }
