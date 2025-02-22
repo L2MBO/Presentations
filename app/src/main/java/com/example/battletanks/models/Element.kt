328a7abc46eeca6b166c5fd0ee0a3ddfcf3f97e8
VechnoMolodoy123 <163846431+VechnoMolodoy123@users.noreply.github.com>
2025-02-22 12:57:16 +0300

    11

--- app/src/main/java/com/example/battletanks/models/Element.kt
+++ app/src/main/java/com/example/battletanks/models/Element.kt
@@ -1,7 +1,13 @@
 package com.example.battletanks.models
+
 import com.example.battletanks.enums.Material
+
 data class Element(
-    val viewId:Int,
+    val viewId: Int,
     val material: Material,
-    val coordinate: Coordinate
-)
+    val coordinate: Coordinate,
+    val width: Int,
+    val height: Int
+) {
+
+}
